d7092d5222fe0aba4100360dc5c2065cc085ef2b
Kokyo Sugiyama <kok.sugiyama@gmail.com>
2024-11-03 23:22:26 +0900

    Initial Commit

---
+++ gymly/gymlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  gymlyApp.swift
+//  gymly
+//
+//  Created by 杉山誇京 on 2024/11/03.
+//
+
+import SwiftUI
+
+@main
+struct gymlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
